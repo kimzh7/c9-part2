80daf03bfce614a5a5c0f8a5df1958a33c660a37
kimzh7 <101956978+kimzh7@users.noreply.github.com>
2022-03-19 21:58:09 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+ box= createSprite(200,200,20,20);
+}
+
+function draw() 
+
+{
+  background(30);
+  if (keyIsDown(RIGHT_ARROW)){
+box.x=box.x+5;
+  }
+  
+  drawSprites();
+  
+}
+
+
+
+
